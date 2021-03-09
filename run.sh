0d8d14c6867f60ea9dc647ed5ba335383d557fda
Alexandru Mahmoud <amahmou4@jhu.edu>
2021-03-09 16:12:53 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -18,6 +18,25 @@ CHARTS_REMOTE="https://$GITHUB_ACTOR:$CHARTS_TOKEN@github.com/$CHARTS_REPO.git"
 BASE_DIR=$(dirname $(pwd))
 CHARTS_DIR=$(basename "$CHARTS_REPO")
 
+BUMP_AWK=$(cat <<EOF
+/[0-9]+\./ {
+  n = split(versionDiff, versions, ".")
+  if(n>NF) nIter=n; else nIter=NF
+  lastNonzero = nIter
+  for(i = 1; i <= nIter; ++i) {
+    if(int(versions[i]) > 0) {
+      lastNonzero = i
+    }
+    $i = versions[i] + $i
+  }
+  for(i = lastNonzero+1; i <= nIter; ++i) {
+    $i = 0
+  }
+  print
+}
+EOF
+)
+
 # exit on error
 set -e
 
@@ -47,7 +66,7 @@ bump_version() {
   # the chart 
   echo "Bumping version"
   # source: https://stackoverflow.com/a/64933139
-  new_version=$(awk -v versionDiff="$bump" -F. -f bump.awk OFS=. <<< "$version")
+  new_version=$(awk -v versionDiff="$bump" -F. "$BUMP_AWK" OFS=. <<< "$version")
   sed -i "s/^version: .\+/version: $new_version/" "$CHART_FILE"
 }
 
